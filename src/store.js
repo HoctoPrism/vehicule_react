b99f87daa62e55c563495932287fcf165952e8e9
unknown <nemexxix@gmail.com>
2022-06-20 20:22:41 +0200

    redux store added

--- src/store.js
+++ src/store.js
@@ -1,8 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit'
+import {applyMiddleware, configureStore} from '@reduxjs/toolkit'
 import {loginButtonSlice} from "./component/features/loginButton/loginButtonSlice";
+import TirageArraySlice from "./component/features/TirageArray/TirageArraySlice";
+import thunk from 'redux-thunk'
 
 export default configureStore({
   reducer: {
-    loggedIn: loginButtonSlice
+    loggedIn: loginButtonSlice,
+    TirageArray: TirageArraySlice
   },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk)
 })
