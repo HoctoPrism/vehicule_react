1371a9cbfcd98397d9722f573389151857938fa7
unknown <nemexxix@gmail.com>
2022-06-20 20:23:22 +0200

    clean app.js

--- src/App.js
+++ src/App.js
@@ -1,67 +1,16 @@
 import './App.css';
-import React, {useEffect} from "react";
+import React from "react";
 import {Outlet} from "react-router-dom";
-import {AppBar, Box, Container, createTheme, CssBaseline, Tabs, ThemeProvider} from "@mui/material";
-import {lightTheme} from "./component/_partials/_theme/_lightTheme";
-import {darkTheme} from "./component/_partials/_theme/_darkTheme";
-import {ColorContext} from "./component/_partials/_theme/_colorContext";
-import {SwitchModeButton} from "./component/_partials/_theme/_switchModeButton";
-import LinkTab from "./assets/js/_navbar";
+import {Box, Container} from "@mui/material";
 
 function App() {
 
-    const [mode, setMode] = React.useState("light");
-    const colorMode = React.useMemo(
-        () => ({
-            toggleColorMode: () => {
-                setMode((prevMode) =>
-                    prevMode === "light" ? "dark" : "light"
-                );
-            },
-        }),
-        []
-    );
-    const theme = React.useMemo(
-        () => createTheme(mode === "light" ? lightTheme : darkTheme),
-        [mode]
-    );
+    document.title = "Accueil"
 
-    function Navbar() {
+    return <Container maxWidth="xl" sx={{ mt: 18 }}>
+        <Outlet/>
+    </Container>
 
-        document.title = "Accueil"
-
-        const [value, setValue] = React.useState(0);
-        const handleChange = (event, newValue) => {
-            return setValue(newValue);
-        };
-
-        useEffect(() => {
-        })
-
-        return (
-            <Box sx={{flexGrow: 1}}>
-                <AppBar className='header'>
-                    <Box sx={{m: 5}} component="div">{document.title}</Box>
-                    <Tabs value={value} onChange={handleChange} aria-label="navbar">
-                        <LinkTab label="Accueil" href="/"/>
-                        <LinkTab label="Page Two" href="/"/>
-                        <LinkTab label="Page Three" href="/"/>
-                        <SwitchModeButton/>
-                    </Tabs>
-                </AppBar>
-            </Box>
-        )
-    }
-
-    return <ColorContext.Provider value={colorMode}>
-        <ThemeProvider theme={theme}>
-            <CssBaseline enableColorScheme/>
-            <Navbar/>
-            <Container maxWidth="xl">
-                <Outlet/>
-            </Container>
-        </ThemeProvider>
-    </ColorContext.Provider>
 }
 
 export default App;
